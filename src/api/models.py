f9a5e9b8058e64c048429e2d72cfa417a76c3bbc
Justice Rivera <142175716+justicetrace@users.noreply.github.com>
2024-08-19 21:40:53 +0000

    jwt

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 db = SQLAlchemy()
 
 class User(db.Model):
